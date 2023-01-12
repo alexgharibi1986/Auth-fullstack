159c4d9080a242bfca655a1aedad6607c9752a4f
alexgharibi1986 <alex.gharibi@demandscience.com>
2023-01-12 14:21:24 -0500

    connect apollo client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,18 @@
-import React from "react";
-import logo from "./logo.svg";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "@apollo/client";
 
 function App() {
+  const { data, loading } = useQuery(gql`
+    {
+      alex
+    }
+  `);
+
+  if (loading) {
+    return <div>loading</div>;
+  }
+
+  console.log(data);
   return <div>helooooo</div>;
 }
 
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { apolloClient } from "./client/apolloClient";
 import "./index.css";
 import App from "./App";
 
@@ -7,7 +9,7 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={apolloClient}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
