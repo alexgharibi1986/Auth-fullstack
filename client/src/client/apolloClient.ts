5237d4dd7b384b2efe13e501c3101813b4e4bc64
alexgharibi1986 <alex.gharibi@demandscience.com>
2023-01-13 16:37:55 -0500

    add apollo refresh token lib

--- client/src/client/apolloClient.ts
+++ client/src/client/apolloClient.ts
@@ -1,6 +1,13 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+  ApolloLink,
+} from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
-import { getAccessToken } from "../auth/accessToken";
+import { TokenRefreshLink } from "apollo-link-token-refresh";
+import { getAccessToken, setAccessToken } from "../auth/accessToken";
+import jwtDecode, { JwtPayload } from "jwt-decode";
 
 const httpLink = createHttpLink({
   uri: "http://localhost:5000/graphql",
@@ -16,8 +23,45 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+//Documentation for this library : https://www.npmjs.com/package/apollo-link-token-refresh
+const refreshToken = new TokenRefreshLink({
+  accessTokenField: "accessToken",
+  isTokenValidOrUndefined: () => {
+    const token = getAccessToken();
+
+    if (!token) {
+      return true;
+    }
+
+    try {
+      const { exp } = jwtDecode<JwtPayload>(token);
+
+      if (Date.now() >= exp! * 1000) {
+        return false;
+      } else {
+        return true;
+      }
+    } catch (err) {
+      return false;
+    }
+  },
+  fetchAccessToken: () => {
+    return fetch("http://localhost:5000/token_refresh", {
+      method: "POST",
+      credentials: "include",
+    });
+  },
+  handleFetch: (accessToken) => {
+    setAccessToken(accessToken);
+  },
+  handleError: (err) => {
+    console.warn("Your refresh token is invalid. Try to relogin");
+    console.error(err);
+  },
+});
+
 export const apolloClient = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: ApolloLink.from([refreshToken, authLink, httpLink]),
   cache: new InMemoryCache(),
   credentials: "include",
 });
