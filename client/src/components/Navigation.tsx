1b4903ca2bf5a45b90b2de06404c2d5038ad0658
alexgharibi1986 <alex.gharibi@demandscience.com>
2023-01-14 11:07:02 -0500

    logout func, add path to cookie

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,12 +1,23 @@
 import { Link } from "react-router-dom";
+import { setAccessToken } from "../auth/accessToken";
+import { useLogoutMutation } from "../generated/graphql";
 
 const Navigation = () => {
+  const [logout, { client }] = useLogoutMutation();
+
+  const handleLogout = async () => {
+    await logout();
+    setAccessToken("");
+    await client.resetStore();
+  };
+
   return (
     <div>
       <Link to="/">Home</Link>
       <Link to="/signup">Sign up</Link>
       <Link to="/login">Login</Link>
       <Link to="/auth">Auth Test</Link>
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 };
--- server/src/util/sendRefreshToken.ts
+++ server/src/util/sendRefreshToken.ts
@@ -3,5 +3,6 @@ import { Response } from "express";
 export const sendRefreshToken = (res: Response, token: string) => {
   res.cookie("awsj", token, {
     httpOnly: true,
+    path: "/token_refresh",
   });
 };
